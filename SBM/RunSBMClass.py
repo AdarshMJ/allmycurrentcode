e6399c292dde3453d2adda9b7c5410a05fe13982
adarshmj <adarsh.jam@gmail.com>
2023-11-29 12:46:56 +0100

    Update RunSBMClass.py
    Includes Munkres algorithm for cluster assignment as accuracy metric from Total Variational Graph Neural Networks paper.

--- SBM/RunSBMClass.py
+++ SBM/RunSBMClass.py
@@ -9,20 +9,21 @@ from torch_geometric.data import Data
 from torch_geometric.utils import to_undirected,to_networkx,from_networkx
 from torch_geometric.transforms import RandomNodeSplit
 from sklearn.model_selection import train_test_split
+from metrics import cluster_acc
 import networkx as nx
 from nodeli import *
 from tqdm import tqdm
 import argparse
 import random
 import numpy as np
-
-
+import csv
 random.seed(42)
 np.random.seed(42)
 torch.manual_seed(1234567)
 
 parser = argparse.ArgumentParser(description='Run SBM')
 parser.add_argument('--path', type=str, default='Dataset/', help='path to directory containing npz file')
+parser.add_argument('--csvout', type=str, default='CommunityAlign/metrics.csv', help='CSV filename to record metrics')
 parser.add_argument('--SBMPlotName', type=str, default='OriginalSBM', help='Saving the SBM plot')
 parser.add_argument('--EmbeddingPlotBefore', type=str, default='OriginalPlotBefore', help='Saving the node embeddings before training')
 parser.add_argument('--EmbeddingPlotAfter', type=str, default='OriginalPlotAfter', help='Saving the node embeddings after training')
@@ -57,136 +58,133 @@ def get_graph_and_labels_from_pyg_dataset(dataset):
     return graph, labels
 
 
+filename = args.csvout   
+
+
 dataset,_ = torch.load(args.path)
 x = dataset['x']
 edge_index = dataset['edge_index']
 y = dataset['y']
 SBMdata = Data(x=x,edge_index=edge_index,y=y,num_classes=2)
 print(SBMdata)
+
 graph, labels = get_graph_and_labels_from_pyg_dataset(SBMdata)
 print("Calculating Different Informativeness Measures...")
-print(f'node label informativeness: {li_node(graph, labels):.2f}')
-print(f'adjusted homophily: {h_adj(graph, labels):.2f}')
-print(f'edge label informativeness: {li_edge(graph, labels):.2f}')
+nodeli = li_node(graph, labels)
+edgeli = li_edge(graph, labels)
+hadj = h_adj(graph, labels)
+print(f'node label informativeness: {nodeli:.2f}')
+print(f'adjusted homophily: {hadj:.2f}')
+print(f'edge label informativeness: {edgeli:.2f}')
 print("=============================================================")
 print()
 
 print("Performing Train/Test splits...")
-transform2 = RandomNodeSplit(split="train_rest", num_test=20)
-SBMdata  = transform2(SBMdata)
-print(SBMdata)
-print(f'Number of training nodes: {SBMdata.train_mask.sum()}')
-print(f'Number of testing nodes: {SBMdata.test_mask.sum()}')
-print(SBMdata.test_mask)
-# print("Train Data:")
-# print(train_data)
-# print("\nTest Data:")
-# print(test_data)
+split = torch.randperm(SBMdata.num_nodes)
+samples = int(0.8*len(split))
+train_idx = split[:samples]
+test_idx = split[samples:]
+print(f"Number of training nodes - {len(train_idx)}")    
+print(f"Number of testing nodes - {len(test_idx)}")     
 
 gsbm = to_networkx(SBMdata,to_undirected=True)
 print(gsbm)
 pos = nx.kamada_kawai_layout(gsbm)
 node_colors = [SBMdata.y[node] for node in gsbm.nodes]
-nx.draw(gsbm, pos=pos, with_labels=False, node_color=node_colors, cmap=plt.cm.rainbow)
+nx.draw(gsbm, pos=pos, with_labels=False, node_color=node_colors, cmap="Set2")
 plt.savefig("Plots/Original/SBMFlipy/OriginalSBMFlipy.jpg")
 
 
 
-# class GCN(torch.nn.Module):
-#     def __init__(self, hidden_channels):
-#         super().__init__()
-#         torch.manual_seed(1234567)
-#         #self.conv1 = GCNConv(newdata.num_features, hidden_channels)
-#         #self.conv2 = GCNConv(hidden_channels, newdata.num_classes)
-#         self.conv1 = GCNConv(train_data.num_features, hidden_channels)
-#         self.conv2 = GCNConv(hidden_channels, SBMdata.num_classes)
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        torch.manual_seed(1234567)
+        #self.conv1 = GCNConv(newdata.num_features, hidden_channels)
+        #self.conv2 = GCNConv(hidden_channels, newdata.num_classes)
+        self.conv1 = GCNConv(SBMdata.num_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, SBMdata.num_classes)
 
-#     def forward(self, x, edge_index):
-#         x = self.conv1(x, edge_index)
-#         x = x.relu()
-#         x = F.dropout(x, p=0.5, training=self.training)
-#         x = self.conv2(x, edge_index)
-#         return x
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
 
-# model = GCN(hidden_channels=512)
-# print(model)
-
-
-# train_losses = []
-
-# print("Visualizing the node embeddings before training...")
-# model.eval()
-# out = model(train_data.x, train_data.edge_index)
-# visualize(out, color=train_data.y,legend_labels=["Class 1", "Class 2"],title='Embeddings before training',filename="Plots/Original/SBMFlipy/BeforeTrainingSBMFlipy.jpg")
-
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
-# criterion = torch.nn.CrossEntropyLoss()
+model = GCN(hidden_channels=512)
+print(model)
 
 
-# train_losses = []
-# nmi_values = []
-# def train():
-#     model.train()
-#     optimizer.zero_grad()  # Clear gradients.
-#     out = model(train_data.x, train_data.edge_index)  # Perform a single forward pass.
 
-#     # Use the CrossEntropyLoss directly without masking here
-#     loss = criterion(out, train_data.y)
+print("Visualizing the node embeddings before training...")
+model.eval()
+out = model(SBMdata.x, SBMdata.edge_index)
+visualize(out, color=SBMdata.y,legend_labels=["Class 1", "Class 2"],title='Embeddings before training',filename="Plots/Original/SBMFlipy/BeforeTrainingSBMFlipy.jpg")
 
-#     # Compute the gradients and update parameters
-#     loss.backward()
-#     optimizer.step()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+criterion = torch.nn.CrossEntropyLoss()
 
-#     return loss.item()
 
-# @torch.no_grad()
-# def test():
-#     model.eval()
-#     clust = model(test_data.x, test_data.edge_index)
-#     return NMI(clust.max(1)[1].cpu(), test_data.y.cpu())
+train_losses = []
+nmi_values = []
+def train():
+    model.train()
+    optimizer.zero_grad()  # Clear gradients.
+    out = model(SBMdata.x, SBMdata.edge_index)  # Perform a single forward pass.
 
-# for epoch in range(1, 1001):
-#     train_loss = train()
-#     train_losses.append(train_loss)
-# nmi = test()
-# print(f'Epoch: {epoch:03d}, Loss: {train_loss:.4f}, ' f'NMI: {nmi:.3f}')
+    # Use the CrossEntropyLoss directly without masking here
+    loss = criterion(out[train_idx], SBMdata.y[train_idx])
 
-# # Plotting the training loss
-# plt.figure(figsize=(10, 5))
-# plt.plot(train_losses, label='Training Loss')
-# plt.title('Training Loss Over Epochs')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.legend()
-# plt.savefig("Plots/MaximizeGap/SBMaxallsame/LossSBMallsame.png")
+    # Compute the gradients and update parameters
+    loss.backward()
+    optimizer.step()
 
-# # Plotting the NMI values
-# # plt.figure(figsize=(10, 5))
-# # plt.plot(nmi_values, label='NMI')
-# # plt.title('NMI Over Epochs')
-# # plt.xlabel('Epoch')
-# # plt.ylabel('NMI')
-# # plt.legend()
-# # plt.savefig("Plots/MaximizeGap/SBMaxallsame/TestNMISBMallsame.png")
-# print("After training GCN, visualizing node embeddings...")
-# model.eval()
-# out = model(test_data.x, test_data.edge_index)
-# visualize(out, color=test_data.y,legend_labels=["Class 1", "Class 2"],title=f'Embeddings after training - NMI: {nmi:.3f}',filename = 'Plots/Original/SBMFlipy/Testsetemb.jpg')
+    return loss.item()
 
-# # Plotting the training loss
-# plt.figure(figsize=(10, 5))
-# plt.plot(train_losses, label='Training Loss')
-# plt.title('Training Loss Over Epochs')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.legend()
-# plt.savefig("Plots/Original/SBMFlipy/LossSBMFlipy.png")
+@torch.no_grad()
+def test():
+    model.eval()
+    clust = model(SBMdata.x, SBMdata.edge_index)
+    return NMI(SBMdata.y[test_idx].cpu(), out[test_idx].max(1)[1].cpu()),cluster_acc(SBMdata.y.cpu().numpy(), clust.max(1)[1].cpu().numpy())[0]
 
-# # Plotting the NMI values
-# # plt.figure(figsize=(10, 5))
-# # plt.plot(test_values, label='Test Accuarcy')
-# # plt.title('Test Accuracy Over Epochs')
-# # plt.xlabel('Epoch')
-# # plt.ylabel('Test Acc')
-# # plt.legend()
-# # plt.savefig("Plots/Original/SBMFlipy/TestNMISBMFlipy.png")
+patience = 50
+best_loss = 1
+nmi_at_best_loss = 0
+acc_at_best_loss = 0
+for epoch in range(1, 1001):
+    train_loss = train()
+    train_losses.append(train_loss)
+    nmi, acc = test()
+    print(f"Epoch: {epoch:03d}, Loss: {train_loss:.4f}, NMI: {nmi:.3f}, ACC: {acc*100: .3f}")
+    if train_loss < best_loss:
+        best_loss = train_loss
+        nmi_at_best_loss = nmi
+        acc_at_best_loss = acc
+        patience = 50
+    else:
+        patience -= 1
+    if patience == 0:
+        break
+print(f"NMI: {nmi_at_best_loss:.3f}, ACC: {acc_at_best_loss*100:.1f}")
+print("===============================================================")
+
+plt.figure(figsize=(10, 5))
+plt.plot(train_losses, label='Training Loss')
+plt.title('Training Loss Over Epochs')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig("Plots/Original/SBMFlipy/Lossoriginal.png")
+
+print("After training GCN, visualizing node embeddings...")
+model.eval()
+out = model(SBMdata.x, SBMdata.edge_index)
+visualize(out[test_idx], color=SBMdata.y[test_idx],legend_labels=["Class 1", "Class 2"],title=f'After training - NMI: {nmi:.3f} ACC: {acc_at_best_loss*100:.1f}',filename = 'Plots/Original/SBMFlipy/Testsetemb.jpg')
+print("Writing Metrics...")
+headers = ['Dataset','NodeLI','EdgeLI','AdjHomophily','TrainingLoss','NMI','TestAcc']
+with open(filename, mode='a', newline='') as file:
+              writer = csv.writer(file)
+              if file.tell() == 0:
+                      writer.writerow(headers)
+              writer.writerow([args.path,nodeli,edgeli,hadj,train_loss,nmi,acc])
